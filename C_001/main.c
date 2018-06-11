725c3002fa543085de31d6ee7c029e978e46c105
Glenda Mullan <Glenda@glendas-imac-2.local>
2018-06-11 11:13:13 +1000

    Initial Commit

---
+++ C_001/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  C_001
+//
+//  Created by Glenda Mullan on 11/6/18.
+//  Copyright © 2018 Glenda Mullan. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
